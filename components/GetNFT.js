843567b24f0121e2dbfc673865c226bf5435aa8d
Gurpreet <gs109690@gmail.com>
2023-04-05 19:39:35 -0500

    Making it so user can add any NFT contract address to get the results

--- components/GetNFT.js
+++ components/GetNFT.js
@@ -6,11 +6,12 @@ import { ethers } from "ethers"
 
 // Contract Details
 import ContractABI from "./ContractAbi.json"
-const Address = "0x82C8C6231E7a4c40d014cb426a49B42863524C88"
+// const Address = "0x82C8C6231E7a4c40d014cb426a49B42863524C88"
 
 const GetNFT = ({}) => {
   const pageSize = 8
   // All State Variables
+  const [address, setAddress] = useState("")
   const [pageNumber, setPageNumber] = useState(1)
   const [nfts, setNfts] = useState([])
   const [openModel, setOpenModel] = useState(false)
@@ -25,7 +26,7 @@ const GetNFT = ({}) => {
       const provider = new ethers.BrowserProvider(window.ethereum)
 
       // Ethers contract instance
-      const nftContract = new ethers.Contract(Address, ContractABI, provider)
+      const nftContract = new ethers.Contract(address, ContractABI, provider)
 
       return nftContract
     }
@@ -59,10 +60,6 @@ const GetNFT = ({}) => {
     }
   }
 
-  useEffect(() => {
-    loadNfts(pageNumber)
-  }, [pageNumber])
-
   function handleNewPage() {
     setPageNumber(pageNumber + 1)
     setSelectedNftDetails(null)
@@ -71,18 +68,36 @@ const GetNFT = ({}) => {
   return (
     <div className="py-8 w-full">
       <div>
-        <Link
-          href={`https://etherscan.io/address/${Address}`}
+        {/* <Link
+          href={`https://etherscan.io/address/${address}`}
           target="_blank"
           rel="noopener noreferrer"
         >
           <p className="text-center text-sm text-zinc-400 hover:text-white transform duration-300 w-[50%] m-auto">
-            {Address}
+            {address}
           </p>
-        </Link>
+        </Link> */}
+        <div className=" w-[60%] m-auto flex items-center mt-10 justify-center gap-8 ">
+          <input
+            className=" placeholder:text-zinc-800 block bg-white w-full border border-slate-300 rounded-md py-2 pl-9 pr-3 shadow-sm focus:outline-none focus:border-zinc-300 focus:ring-zinc-300 focus:ring-1 sm:text-sm text-zinc-800"
+            placeholder="Contract Address"
+            type="text"
+            name="search"
+            defaultValue={address}
+          />
+          <button
+            onClick={() =>
+              setAddress(document.getElementsByName("search")[0].value)
+            }
+            className="bg-black/30 hover:bg-zinc-200 transform duration-300  text-white hover:shadow-lg hover:shadow-black hover:text-zinc-800 font-semibold py-1 px-8 border border-white rounded-lg"
+          >
+            Set Address
+          </button>
+        </div>
+
         <div className=" flex items-center w-full mt-10 justify-center ">
           <button
-            onClick={handleNewPage}
+            onClick={() => loadNfts(pageNumber)}
             className="bg-black/30 hover:bg-zinc-200 transform duration-300  text-white hover:shadow-lg hover:shadow-black hover:text-zinc-800 font-semibold py-2 px-4 border border-white rounded-lg"
           >
             Load NFT's
